6b464ee752906c7b8302e2fa35464346399130b3
vamsikrishnabandari <bandarivamsi@gmail.com>
2021-08-03 11:04:57 -0400

    initial commit of base python file with defined coordinate variables

---
+++ main.py
@@ -0,0 +1,13 @@
+#Created on 03/08/2021.
+#Author: vamsikrishna bandari
+x = 0
+y = 0
+xpos = 'True'
+ypos = 'True'
+
+#Here xpos and ypos are coordinates
+
+def checkleft(xpos,ypos):
+    if xpos == 'stop' and ypos == 'True':
+        xpos = 'False Stop'
+        return xpos

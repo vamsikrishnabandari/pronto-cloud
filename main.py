e45cf7d532de5cbedf3a592d06ce2fa6d13e646c
vamsikrishnabandari <bandarivamsi@gmail.com>
2021-08-03 11:55:14 -0400

    rotateleft and rotateright methods are defined

--- main.py
+++ main.py
@@ -8,9 +8,51 @@ ypos = 'True'
 #Here xpos and ypos are coordinates
 
 def checkleft(xpos,ypos):
-    if xpos == 'stop' and ypos == 'True':
+    if xpos == 'Stop' and ypos == 'True':
         xpos = 'False Stop'
         return xpos
-    elif xpos == 'False' and ypos == 'stop'
-        ypos = 'True stop'
+    elif xpos == 'False' and ypos == 'Stop':
+        ypos = 'Stop False'
+        return ypos
+    elif xpos == 'Stop' and ypos == 'False':
+        xpos = 'True stop'
+        return xpos 
+    elif xpos == 'True' and ypos == 'Stop':
+        ypos = 'Stop True'
+        return ypos
+
+def checkright(xpos,ypos):
+    if xpos == 'Stop' and ypos == 'True':
+        xpos = 'True Stop'
         return xpos
+    elif xpos == 'True' and ypos == 'Stop':
+        ypos = 'Stop False'
+        return ypos
+    elif xpos == 'Stop' and ypos == 'False':
+        xpos = 'False stop'
+        return xpos 
+    elif xpos == 'False' and ypos == 'Stop':
+        ypos = 'Stop True'
+        return ypos
+
+def rotateleft(lx,ly,count):
+    if lx == 'Stop' and ly =='True':
+        return count
+    else:
+        res = checkleft(lx,ly)
+        resp = res.split(' ')
+        lx = resp[0]
+        ly = resp[1]
+        count = count + 1
+        return rotateleft(lx,ly,count)
+
+def rotateright(rx,ry,count):
+    if rx == 'Stop' and ry =='True':
+        return count
+    else:
+        res = checkright(rx,ry)
+        resp = res.split(' ')
+        rx = resp[0]
+        ry = resp[1]
+        count = count + 1
+        return rotateright(rx,ry,count)
